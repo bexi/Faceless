606bfbb934c17d285a3fdfd13266def7542b3757
bexi <trreitmaier@msn.com>
2016-05-14 15:54:21 +0200

    added spinner in code and xml

--- app/src/main/java/com/example/rebeckareitmaier/faceless/ProfileActivity.java
+++ app/src/main/java/com/example/rebeckareitmaier/faceless/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.example.rebeckareitmaier.faceless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,5 +16,15 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Categories, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
 }
