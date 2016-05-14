52e2fe4f8f42e9028c08166f04630f1550a9cd3a
Tinge-Ling <miss_amtl@live.com>
2016-05-14 17:04:38 +0200

    toolbar in profile

--- app/src/main/java/com/example/rebeckareitmaier/faceless/ProfileActivity.java
+++ app/src/main/java/com/example/rebeckareitmaier/faceless/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.example.rebeckareitmaier.faceless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
@@ -16,6 +18,8 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
     // Create an ArrayAdapter using the string array and a default spinner layout
@@ -25,6 +29,26 @@ public class ProfileActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        ImageButton homeBtn = (ImageButton) toolbar.findViewById(R.id.homeBtn);
+        ImageButton forumBtn = (ImageButton) toolbar.findViewById(R.id.forumBtn);
+
+        forumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextScreen = new Intent(ProfileActivity.this, ForumActivity.class);
+                startActivity(nextScreen);
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextScreen = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(nextScreen);
+
+            }
+        });
     }
 
 }
