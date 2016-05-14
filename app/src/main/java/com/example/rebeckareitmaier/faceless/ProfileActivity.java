67a5f474e1d51a61c42fe413369a033a4b9a6734
bexi <trreitmaier@msn.com>
2016-05-14 10:19:43 +0200

    added imagebutton to profileActivity

--- app/src/main/java/com/example/rebeckareitmaier/faceless/ProfileActivity.java
+++ app/src/main/java/com/example/rebeckareitmaier/faceless/ProfileActivity.java
@@ -2,6 +2,10 @@ package com.example.rebeckareitmaier.faceless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,4 +14,20 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public void addListenerOnButton() {
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(MyAndroidAppActivity.this,
+                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
 }
